6169fc65ee44378325b514b4211467833477edb1
universenox <khswanson24@gmail.com>
2019-07-16 11:27:40 -0400

    pi using hardcoded digits

--- bench/main_bench.cpp
+++ bench/main_bench.cpp
@@ -1,7 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <real/real.hpp>
 
-std::optional<size_t> boost::real::real::maximum_precision = 10;
-std::optional<size_t> boost::real::real_algorithm::maximum_precision = 10;
+std::optional<size_t> boost::real::const_precision_iterator::maximum_precision = 10;
 
 BENCHMARK_MAIN();
